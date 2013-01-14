90fac29a2e26d58c3e43f9ece1f7b4bc60b2afa2
Yen-Ju Chen <yjchenx@gmail.com>
2013-01-14 16:52:10 +0800

    test tagger

--- app/models/simple_tag/tag.rb
+++ app/models/simple_tag/tag.rb
@@ -1,7 +1,6 @@
 class SimpleTag::Tag < ActiveRecord::Base
   has_many :taggings, :dependent => :destroy, 
                       :class_name => 'SimpleTag::Tagging'
-  has_many :taggables, :through => :taggings
   has_many :taggers, :through => :taggings
 
   # Setup accessible (or protected) attributes for your model
--- spec/dummy/spec/models/simple_tag/tag_spec.rb
+++ spec/dummy/spec/models/simple_tag/tag_spec.rb
@@ -2,6 +2,39 @@ require 'spec_helper'
 
 describe SimpleTag do
   describe SimpleTag::Tag do
+    it 'can get taggable' do
+      post = Post.create(:name => 'post')
+      comment = Comment.create(:name => 'comment')
+
+      post.set_tags('ruby, jruby')
+      comment.set_tags('jruby, java')
+
+      SimpleTag::Tag.pluck(:name).should match_array(['ruby', 'jruby', 'java'])
+
+      tag = SimpleTag::Tag.where(:name => 'ruby').first
+      tag.taggings.collect(&:taggable).collect(&:name).should match_array(['post'])
+
+      tag = SimpleTag::Tag.where(:name => 'jruby').first
+      tag.taggings.collect(&:taggable).collect(&:name).should match_array(['post', 'comment'])
+
+      post.set_tags('ruby, rvm')
+      tag = SimpleTag::Tag.where(:name => 'jruby').first
+      tag.taggings.collect(&:taggable).collect(&:name).should match_array(['comment'])
+    end
+
+    it 'can get tagger' do
+      post = Post.create(:name => 'post')
+      user = User.create(:name => 'bob')
+      
+      post.set_tags 'ruby, rvm', :tagger => user
+      post.set_tags 'jruby, java'
+
+      tag = SimpleTag::Tag.where(:name => 'ruby').first
+      tag.taggers.pluck(:name).should match_array(['bob'])
+      tag = SimpleTag::Tag.where(:name => 'java').first
+      tag.taggers.pluck(:name).should be_empty
+    end
+
     it 'can create tag' do
       tag = SimpleTag::Tag.create(:name => 'tag')
       tag.should_not be_nil
