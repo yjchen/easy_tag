a89ce4000955f64574a8ba4c854b3f600d4ff119
Yen-Ju Chen <yjchenx@gmail.com>
2013-07-15 16:10:49 +0800

    migrating to Rails 4

--- spec/dummy/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  acts_as_taggable
-
-  attr_accessible :name
-end
--- spec/dummy/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  acts_as_tagger
-
-  attr_accessible :name
-end
